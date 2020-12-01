6a71d00aa484a20a6e2e9d961875e1606758a369
Niels Vrolijk <niels@nielsvrolijk.nl>
2020-12-01 13:05:57 +0100

    Added comment

--- AdventOfCode/01-C/Program.cs
+++ AdventOfCode/01-C/Program.cs
@@ -10,13 +10,17 @@ namespace _01_C
         static IEnumerable<int> readInts(string filePath) => readlines(filePath).Select(s => Convert.ToInt32(s));
         static IEnumerable<Tuple<int,int>> Pairs(IEnumerable<int> list)
         {
+            //Assume [A,B,C,D,E]
             if (!list.Any())
             {
+                //return empty Tuple
                 return Enumerable.Empty<Tuple<int, int>>();
             }
             var head = list.First();
             var tail = list.Skip(1);
-            return tail.Select(t => new Tuple<int, int>(head, t)).Concat(Pairs(tail));
+            return tail.Select(t => new Tuple<int, int>(head, t)) //Return all the possbile combinations you can make with head and tail elements
+                                                                  // (A,B),(A,C),(A,D),(A,E)
+                .Concat(Pairs(tail)); //Return all pair combinations possible from the tail, is Pair([B,C,D,E])
         }
 
         static void Main(string[] args)
--- AdventOfCode/02/Program.cs
+++ AdventOfCode/02/Program.cs
@@ -10,27 +10,39 @@ namespace _02
         static IEnumerable<int> readInts(string filePath) => readlines(filePath).Select(s => Convert.ToInt32(s));
         static IEnumerable<Tuple<int, int>> Pairs(IEnumerable<int> list)
         {
-            if (!list.Any())
             {
-                return Enumerable.Empty<Tuple<int, int>>();
+                //Assume [A,B,C,D,E]
+                if (!list.Any())
+                {
+                    //return empty Tuple
+                    return Enumerable.Empty<Tuple<int, int>>();
+                }
+                var head = list.First(); // A
+                var tail = list.Skip(1); // [B,C,D,E]
+                return tail.Select(t => new Tuple<int, int>(head, t)) //Return all the possbile combinations you can make with head and tail elements
+                                                                      // (A,B),(A,C),(A,D),(A,E)
+                    .Concat(Pairs(tail)); //Return all pair combinations possible from the tail, is Pair([B,C,D,E])
             }
-            var head = list.First();
-            var tail = list.Skip(1);
-            return tail.Select(t => new Tuple<int, int>(head, t)).Concat(Pairs(tail));
         }
         static IEnumerable<Tuple<int, int, int>> Triplets(IEnumerable<int> list)
         {
+            //Assumbe [A,B,C,D,E]
             if (list.Count()<=2)
             {
+                //return empty Tuple
                 return Enumerable.Empty<Tuple<int, int, int>>();
             }
             if (list.Count() == 3)
             {
+                //Assume called with [A,B,C] only (A,B,C) can be returned
                 return Enumerable.Repeat(new Tuple<int,int,int>(list.First(),list.Skip(1).First(),list.Skip(2).First()),1);
             }
-            var head = list.First();
-            var tail = list.Skip(1);
-            return Pairs(tail).Select(t => new Tuple<int, int, int>(head, t.Item1, t.Item2)).Concat(Triplets(tail));
+            var head = list.First(); //A
+            var tail = list.Skip(1); // [B,C,D,E]
+            return Pairs(tail).Select(t => new Tuple<int, int, int>(head, t.Item1, t.Item2))
+                //Select all possible pairs from tail, and prepend them with head
+                // (A,B,C),(A,B,D), ... , (A,D,E)
+                .Concat(Triplets(tail)); //Return all triple combinations possible from the tail, is Triplets(tail)
         }
 
         static void Main(string[] args)
