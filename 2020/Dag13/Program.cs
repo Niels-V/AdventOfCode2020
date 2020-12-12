e1eb394185a657190624d018413f8d98a16ef107
Niels Vrolijk <niels@nielsvrolijk.nl>
2020-12-12 22:43:10 +0100

    Setup dag 13, plus added empty project

---
+++ 2020/Dag13/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common;
+
+namespace Dag13
+{
+    [TestClass]
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = First("input.txt");
+            Console.WriteLine("Program succes, found result: {0}", result);
+
+            var result2 = Second("input.txt");
+            Console.WriteLine("Program succes, found result: {0}", result2);
+
+        }
+
+        static int First(string inputFile)
+        {
+            return -1;
+        }
+
+        static int Second(string inputFile)
+        {
+            return -2;
+        }
+
+
+        [DataTestMethod]
+        [DataRow("test.txt", 0)]
+        public void TestFirst(string inputFile, int expectedResult)
+        {
+            var result = First(inputFile);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+
+        [DataTestMethod]
+        [DataRow("test.txt", 0)]
+        public void TestSecond(string inputFile, int expectedResult)
+        {
+            var result = Second(inputFile);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
