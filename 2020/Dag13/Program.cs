70616e46b237b936b6a5696ccabe8a421a02ff23
Niels Vrolijk <niels@nielsvrolijk.nl>
2020-12-13 22:36:30 +0100

    Day 13

--- 2020/Dag13/Program.cs
+++ 2020/Dag13/Program.cs
@@ -2,13 +2,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Common;
+using System.Runtime.CompilerServices;
 
 namespace Dag13
 {
+    public struct BusInput
+    {
+        public long BusNumber { get; set; }
+        public long SkipMinutes { get; set; }
+    }
     [TestClass]
     public class Program
     {
+        static IEnumerable<string> Readlines(string filePath) => System.IO.File.ReadLines(filePath);
+
         static void Main(string[] args)
         {
             var result = First("input.txt");
@@ -16,31 +23,123 @@ namespace Dag13
 
             var result2 = Second("input.txt");
             Console.WriteLine("Program succes, found result: {0}", result2);
-
         }
 
-        static int First(string inputFile)
+        static long First(string inputFile)
         {
-            return -1;
+            var input = Readlines(inputFile).ToList();
+            var estimationTime = Convert.ToInt64(input[0]);
+            var busInfo = input[1].Split(",", StringSplitOptions.RemoveEmptyEntries);
+            var busLines = busInfo.Select((bNr, i) => new { Index = i, BusNumber = bNr }).Where(l => l.BusNumber != "x")
+                .Select(s => new { BusNumber = Convert.ToInt64(s.BusNumber), SkipMinutes = s.Index });
+            var waitTimes = busLines.ToDictionary(b => b.BusNumber, b => FindWaitTime(estimationTime, b.BusNumber));
+            var minWaitValue = waitTimes.Values.Min();
+            var busNumber = waitTimes.First(w => w.Value == minWaitValue).Key;
+            return busNumber * minWaitValue;
         }
-
-        static int Second(string inputFile)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static long FindWaitTime(long estimationTime, long busNumber)
         {
-            return -2;
+            return busNumber - (estimationTime % busNumber);
+        }
+    
+        static long Second(string inputFile)
+        {
+            var input = Readlines(inputFile).ToList();
+            var busInfo = input[1].Split(",", StringSplitOptions.RemoveEmptyEntries);
+            return FindMagicTime(busInfo);
+        }
+        /// <summary>
+        /// Solves the solution to
+        /// Ni*ui ≡ 1 % ni
+        /// by brute forcing all combiniations of ui
+        /// The while loop is evaluated max ni times
+        /// </summary>
+        /// <param name="Ni"></param>
+        /// <param name="ni"></param>
+        /// <returns>ui</returns>
+        private static long FindUi(long Ni, long ni)
+        {
+            var ui = 1L;
+            while (true)
+            {
+                var remainder = Ni * ui % ni;
+                if (remainder == 1) { return ui; }
+                ui++;
+            }
         }
 
+        private static long FindMagicTime(IEnumerable<string> busInfo)
+        {
+            //Note that the buslines are all primes
+            //And as this questions are about modulus, the Chinese Remainder Theorem can be applied
+            //See: https://www.dave4math.com/mathematics/chinese-remainder-theorem/
+            // First the bus question needs to transformed from Tfirstbus + skipMins to Tfirstbus
+            // As:  (Tfirstbus + skipMinsBusNr) % BusNr = 0 <=> 
+            //       Tfirstbus % BusNr = -skipMinsBusNr, but result of mod is always between 0 and (busNr-1), so
+            //       Tfirstbus % BusNr = (BusNr-skipMinsBusNr) but on skipMinsBusNr, this should be 0, so Mod with BusNr
+            //       Tfirstbus % BusNr = (BusNr-skipMinsBusNr) % busNr
+            // solution is needed for all busses:
+            // t ≡ (busnr - skipmins % busNr) % busNr 
+            // so ni = busNr
+            //    ai = (busnr - skipmins % busNr)
+            //    Ni = N / ni
+            var busLines = busInfo.Select((bNr, i) => new { Index = i, BusNumber = bNr }).Where(l => l.BusNumber != "x")
+                .Select(s => new BusInput { BusNumber = Convert.ToInt64(s.BusNumber), SkipMinutes = s.Index }).ToList();
+            //N = Σ ni for all busses
+            var N = busLines.Select(b => b.BusNumber).Aggregate(1L, (product, busLine) => product *= busLine);
+            //calculate all the parameters for each bus 
+            var aiNiuis = busLines.Select(b => new { Bus = b, 
+                ui = FindUi(N / b.BusNumber, b.BusNumber), 
+                Ni = N/b.BusNumber,
+                ai = (b.BusNumber - b.SkipMinutes) % b.BusNumber
+            }).ToList();
+            //the first t is now given as: Σ (ai * Ni * ui) (mod N)
+            return aiNiuis.Sum(b => b.ai * b.Ni * b.ui) % N;
+        }
 
         [DataTestMethod]
-        [DataRow("test.txt", 0)]
+        [DataRow(939, 7, 6)]
+        [DataRow(939, 59, 5)]
+        [DataRow(939, 13, 10)]
+        public void TestFindWaitTime(long estimationTime, long busNumber, long expectedResult)
+        {
+            var result = FindWaitTime(estimationTime, busNumber);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [DataTestMethod]
+        [DataRow(28, 3, 1)]
+        [DataRow(21, 4, 1)]
+        [DataRow(12, 7, 3)]
+        public void TestFindUi(long Ni, long ni, long expectedResult)
+        {
+            var result = FindUi(Ni, ni);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [DataTestMethod]
+        [DataRow(new[] { "17", "x", "13", "19" }, 3417)]
+        [DataRow(new[] { "67", "7", "59", "61" }, 754018)]
+        [DataRow(new[] { "67", "x", "7", "59", "61" }, 779210)]
+        [DataRow(new[] { "67", "7", "x", "59", "61" }, 1261476)]
+        [DataRow(new[] { "1789", "37", "47", "1889" }, 1202161486)]
+        public void TestFindMagicTime(string[] busses, long expectedResult)
+        {
+            var result = FindMagicTime(busses);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [DataTestMethod]
+        [DataRow("test.txt", 295)]
         public void TestFirst(string inputFile, int expectedResult)
         {
             var result = First(inputFile);
             Assert.AreEqual(expectedResult, result);
         }
 
-
         [DataTestMethod]
-        [DataRow("test.txt", 0)]
+        [DataRow("test.txt", 1068781)]
         public void TestSecond(string inputFile, int expectedResult)
         {
             var result = Second(inputFile);
---
+++ Common/Enumerable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public static class Enumerable
+    {
+        public static IEnumerable<long> LongRange(long start, long end)
+        {
+            var current = start;
+            while (current < end)
+            {
+                yield return current++;
+            }
+            yield return current;
+        }
+    }
+}
