a0f45a56ba4407c987c404d8cbc260c46d55a3f0
Niels Vrolijk <niels@nielsvrolijk.nl>
2021-12-02 11:16:26 +0100

    Dag2

---
+++ 2021/Dag02/Parser.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Common;
+
+namespace Dag02
+{
+    [TestClass]
+    public class Parser : Parser<Instruction>
+    {
+        static readonly Regex instructionRule = new("^(?<operation>forward|down|up)\\s(?<argument>\\d+)$", RegexOptions.Compiled);
+
+        protected override Instruction ParseLine(string line)
+        {
+            var regexResult = instructionRule.Match(line);
+
+            var instruction = new Instruction
+            {
+                Operation = regexResult.Groups.Values.First(g => g.Name == "operation").Value.ParseEnumValue<OperationType>(),
+                Argument = Convert.ToInt32(regexResult.Groups.Values.First(g => g.Name == "argument").Value)
+            };
+            return instruction;
+        }
+
+        [DataTestMethod]
+        [DataRow("forward 5", OperationType.Forward)]
+        [DataRow("up 5", OperationType.Up)]
+        [DataRow("down 6", OperationType.Down)]
+        [DataRow("forward 13", OperationType.Forward)]
+        public void Test_Regex_Operation(string input, OperationType expectedOperation)
+        {
+            var result = ParseLine(input);
+            Assert.AreEqual(expectedOperation, result.Operation);
+        }
+
+        [DataTestMethod]
+        [DataRow("forward 5", 5)]
+        [DataRow("up 5", 5)]
+        [DataRow("down 6", 6)]
+        [DataRow("forward 13", 13)]
+        public void Test_Regex_Argument(string input, int expectedArgument)
+        {
+            var result = ParseLine(input);
+            Assert.AreEqual(expectedArgument, result.Argument);
+        }
+
+        [TestMethod]
+        public void RegexTest()
+        {
+            foreach (var item in Readlines("test.txt"))
+            {
+                var regExREsult = instructionRule.Match(item);
+                Assert.IsTrue(regExREsult.Success);
+            }
+        }
+    }
+}
---
+++ 2021/Dag02/Program.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common;
+using System.Runtime.Serialization;
+
+namespace Dag02
+{
+    public enum OperationType
+    {
+        [EnumMember(Value = "forward")]
+        Forward,
+        [EnumMember(Value = "down")]
+        Down,
+        [EnumMember(Value = "up")]
+        Up
+    }
+
+    public struct Position
+    {
+        public int X;
+        public int Depth;
+        public int Aim;
+
+        public Position(int x, int depth, int aim)
+        {
+            X = x; Depth = depth; Aim = aim;
+        }
+    }
+
+    public class Instruction
+    {
+        public OperationType Operation { get; set; }
+        public int Argument { get; set; }
+    }
+
+    [TestClass]
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = First("input.txt");
+            Console.WriteLine("Program succes, found result: {0}", result);
+
+            var result2 = Second("input.txt");
+            Console.WriteLine("Program succes, found result: {0}", result2);
+
+        }
+
+        static long First(string inputFile)
+        {
+            var subRoute = new Parser().ReadData(inputFile);
+            Position position = new();
+            return subRoute.Aggregate(position, (oldPosition, instruction) =>
+            {
+                return instruction.Operation switch
+                {
+                    OperationType.Forward => new(oldPosition.X + instruction.Argument, oldPosition.Depth, oldPosition.Aim),
+                    OperationType.Down => new(oldPosition.X, oldPosition.Depth + instruction.Argument, oldPosition.Aim),
+                    OperationType.Up => new(oldPosition.X, oldPosition.Depth - instruction.Argument, oldPosition.Aim),
+                    _ => throw new InvalidOperationException(),
+                };
+            }, position => position.X * position.Depth);
+        }
+
+        static int Second(string inputFile)
+        {
+            var subRoute = new Parser().ReadData(inputFile);
+
+            Position position = new();
+            return subRoute.Aggregate(position, (oldPosition, instruction) =>
+            {
+                return instruction.Operation switch
+                {
+                    OperationType.Forward => new(oldPosition.X + instruction.Argument, oldPosition.Depth + oldPosition.Aim * instruction.Argument, oldPosition.Aim),
+                    OperationType.Down => new(oldPosition.X, oldPosition.Depth, oldPosition.Aim + instruction.Argument),
+                    OperationType.Up => new(oldPosition.X, oldPosition.Depth, oldPosition.Aim - instruction.Argument),
+                    _ => throw new InvalidOperationException(),
+                };
+            }, position => position.X * position.Depth);
+        }
+
+        [TestMethod]
+        public void TestPart1()
+        {
+            var result = First("test.txt");
+            Assert.AreEqual(150, result);
+        }
+
+        [TestMethod]
+        public void TestPart2()
+        {
+            var result = Second("test.txt");
+            Assert.AreEqual(900, result);
+        }
+    }
+}
