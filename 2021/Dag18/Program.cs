c6f9549816341eeb3c1be1af9d4fc92d8fa754c8
Niels Vrolijk <niels@nielsvrolijk.nl>
2021-12-19 21:14:38 +0100

    Dag 18

---
+++ 2021/Dag18/Program.cs
@@ -0,0 +1,194 @@
+﻿using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC
+{
+    [TestCategory("2021")]
+    [TestClass]
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = First("input.txt");
+            Console.WriteLine("Program succes, found result: {0}", result);
+
+            var result2 = Second("input.txt");
+            Console.WriteLine("Program succes, found result: {0}", result2);
+        }
+
+        static long First(string inputFile)
+        {
+            var numbers = new Parser().ParseFile(inputFile);
+            var result = numbers.First();
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                result += numbers[i];
+                ((SnailfishNumberPair)result).Reduce();
+            }
+            return result.Magnitude;
+        }
+
+        static long Second(string inputFile)
+        {
+            var parser = new Parser();
+            var numbers = parser.ParseFile(inputFile);
+            var maxMagnitude = 0L;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                for (int j = 0; j < numbers.Count; j++)
+                {
+                    if (i!=j)
+                    {
+                        var n = Parser.ParseNumber(numbers[i].ToString()) + Parser.ParseNumber(numbers[j].ToString());
+                        n.Reduce();
+                        var magnitude = n.Magnitude;
+                        if (magnitude>maxMagnitude) { maxMagnitude = magnitude; }
+                    }
+                }
+            }
+            return maxMagnitude;
+        }
+
+        [TestMethod]
+        public void TestPart1()
+        {
+            var result = First("test.txt");
+            Assert.AreEqual(4140, result);
+        }
+
+        [TestMethod]
+        public void TestPart2()
+        {
+            var result = Second("test.txt");
+            Assert.AreEqual(3993, result);
+        }
+    }
+
+    [TestCategory("2021")]
+    [TestClass]
+    public class Parser : LineParser
+    {
+        public List<SnailfishNumber> ParseFile(string filePath)
+        {
+            var numbers = new List<SnailfishNumber>();
+            foreach (var line in ReadData(filePath))
+            {
+                SnailfishNumberPair currentPair = ParseNumber(line);
+                numbers.Add(currentPair);
+            }
+            return numbers;
+        }
+
+        internal static SnailfishNumberPair ParseNumber(string numberString)
+        {
+            SnailfishNumberPair currentPair = null;
+            Stack<SnailfishNumberPair> pairs = new Stack<SnailfishNumberPair>();
+            foreach (var letter in numberString)
+            {
+                switch (letter)
+                {
+                    case '[':
+                        //add pair
+                        if (currentPair != null) { pairs.Push(currentPair); }
+                        currentPair = new SnailfishNumberPair();
+                        var parent = pairs.Count > 0 ? pairs.Peek() : null;
+                        if (parent != null)
+                        {
+                            currentPair.Parent = parent;
+                            if (parent?.LeftNumber == null)
+                            {
+                                parent.SetLeft(currentPair);
+                            }
+                            else
+                            {
+                                parent.SetRight(currentPair);
+                            }
+                        }
+                        break;
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        var number = new SnailfishRegularNumber(Convert.ToInt32(letter.ToString()));
+                        if (currentPair?.LeftNumber == null)
+                        {
+                            currentPair.SetLeft(number);
+                        }
+                        else
+                        {
+                            currentPair.SetRight(number);
+                        }
+                        break;
+                    case ']':
+                        currentPair = pairs.Count > 0 ? pairs.Pop() : currentPair;
+                        break;
+                    case ',':
+                    //do nothing, logic is in set number
+                    default:
+                        break;
+                }
+            }
+
+            return currentPair;
+        }
+
+        [TestMethod]
+        public void TestParseByCounts()
+        {
+            var result = ParseFile("test.txt");
+            Assert.AreEqual(10, result.Count);
+        }
+
+        [DataTestMethod]
+        [DataRow("[[1,2],[[3,4],5]]", 143)]
+        [DataRow("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", 1384)]
+        [DataRow("[[[[1,1],[2,2]],[3,3]],[4,4]]", 445)]
+        [DataRow("[[[[3,0],[5,3]],[4,4]],[5,5]]", 791)]
+        [DataRow("[[[[5,0],[7,4]],[5,5]],[6,6]]", 1137)]
+        [DataRow("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", 3488)]
+        [DataRow("[[[[7,8],[6,6]],[[6,0],[7,7]]],[[[7,8],[8,8]],[[7,9],[0,6]]]]", 3993)]
+        public void TestParseMagnitued(string line, long expectedMagnitude)
+        {
+            var result = ParseNumber(line);
+            Assert.AreEqual(expectedMagnitude, result.Magnitude);
+        }
+
+        [DataTestMethod]
+        [DataRow("[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]")]
+        [DataRow("[7,[6,[5,[4,[3,2]]]]]", "[7,[6,[5,[7,0]]]]")]
+        [DataRow("[[6,[5,[4,[3,2]]]],1]", "[[6,[5,[7,0]]],3]")]
+        [DataRow("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[7,0]]]]")]
+        [DataRow("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[7,0]]]]")]
+        public void TestReduce(string number1, string expected)
+        {
+            var a = ParseNumber(number1);
+            a.Reduce();
+            Assert.AreEqual(expected, a.ToString());
+        }
+
+        [DataTestMethod]
+        [DataRow("[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]", "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]", "[[[[7,8],[6,6]],[[6,0],[7,7]]],[[[7,8],[8,8]],[[7,9],[0,6]]]]")]
+        [DataRow("[[[[4,3],4],4],[7,[[8,4],9]]]", "[1,1]", "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")]
+        [DataRow("[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]", "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]", "[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]")]
+        [DataRow("[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]", "[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]", "[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]")]
+        public void TestAdditionWithReduce(string number1, string number2, string expected)
+        {
+            var n1 = ParseNumber(number1);
+            var n2 = ParseNumber(number2);
+            var a = n1 + n2;
+            a.Reduce();
+            Assert.AreEqual(expected, a.ToString());
+        }
+
+    }
+}
---
+++ 2021/Dag18/SnailPair.cs
@@ -0,0 +1,271 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AoC
+{
+    public class SnailfishNumberPair : SnailfishNumber
+    {
+        public SnailfishNumberPair(SnailfishNumber n1, SnailfishNumber n2)
+        {
+            SetLeft(n1);
+            SetRight(n2);
+        }
+        public SnailfishNumberPair()
+        {
+        }
+        public void SetLeft(SnailfishNumber n)
+        {
+            LeftNumber = n;
+            LeftNumber.Parent = this;
+            LeftNumber.NumberPosition = PairPosition.Left;
+        }
+        public void SetRight(SnailfishNumber n)
+        {
+            RightNumber = n;
+            RightNumber.Parent = this;
+            RightNumber.NumberPosition = PairPosition.Right;
+        }
+
+        public SnailfishNumber LeftNumber { get; set; }
+        public SnailfishNumber RightNumber { get; set; }
+
+        //The magnitude of a pair is 3 times the magnitude of its left element plus 2 times the magnitude of its right element.
+        public override long Magnitude => 3*LeftNumber.Magnitude + 2*RightNumber.Magnitude;
+
+        public void Explode()
+        {
+            //To explode a pair, the pair's left value is added to the first regular number to the left of the exploding pair (if any),
+            //and the pair's right value is added to the first regular number to the right of the exploding pair (if any).
+            //Exploding pairs will always consist of two regular numbers. Then, the entire exploding pair is replaced with the regular number 0.
+
+            //Change number to the left
+            var current = this;
+            while (current != null)
+            {
+                if (current.NumberPosition == PairPosition.Right)
+                {
+                    current.Parent.LeftNumber.AddToRight(((SnailfishRegularNumber)LeftNumber).Number);
+                    break;
+                }
+                current = current.Parent;
+            }
+            //Change number to the right
+            current = this;
+            while (current != null)
+            {
+                if (current.NumberPosition == PairPosition.Left)
+                {
+                    current.Parent.RightNumber.AddToLeft(((SnailfishRegularNumber)RightNumber).Number);
+                    break;
+                }
+                current = current.Parent;
+            }
+
+            //Replace this pair with a zero
+            var currentParent = Parent;
+            SnailfishRegularNumber replacement = new SnailfishRegularNumber(0)
+            {
+                Parent = currentParent
+            };
+            if (NumberPosition == PairPosition.Left)
+            {
+                replacement.NumberPosition = PairPosition.Left;
+                replacement.Parent.LeftNumber = replacement;
+            }
+            else
+            {
+                replacement.NumberPosition = PairPosition.Right;
+                replacement.Parent.RightNumber = replacement;
+            }
+        }
+        public void Reduce()
+        {
+            var doExplode = true;
+            var doSplit = true;
+            while (doExplode || doSplit)
+            {
+                while (doExplode)
+                {
+                    doExplode = CheckExplode(1);
+                }
+                doSplit = CheckSplit();
+                if (doSplit) { doExplode = true; }
+            }
+        }
+        public override bool CheckExplode(int pairCount)
+        {
+            //To reduce a snailfish number, you must repeatedly do the first action in this list that applies to the snailfish number:
+            //If any pair is nested inside four pairs, the leftmost such pair explodes.
+            if (pairCount > 4)
+            {
+                Explode();
+                return true;
+            }
+            else
+            {
+                if (LeftNumber.CheckExplode(pairCount + 1))
+                {
+                    return true;
+                }
+                if (RightNumber.CheckExplode(pairCount + 1))
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public override bool CheckSplit() => LeftNumber.CheckSplit() || RightNumber.CheckSplit() || false;
+
+        public override string ToString()
+        {
+            return $"[{LeftNumber},{RightNumber}]";
+        }
+
+        public override void AddToLeft(int number)
+        {
+            LeftNumber.AddToLeft(number);
+        }
+
+        public override void AddToRight(int number)
+        {
+            RightNumber.AddToRight(number);
+        }
+    }
+
+    public class SnailfishRegularNumber : SnailfishNumber
+    {
+        public SnailfishRegularNumber(int n)
+        {
+            Number = n;
+        }
+
+        public int Number { get; set; }
+
+        //The magnitude of a regular number is just that number.
+        public override long Magnitude => Number;
+        public override string ToString()
+        {
+            return Number.ToString();
+        }
+
+        public override bool CheckExplode(int pairCount) => false;
+
+        public override bool CheckSplit()
+        {
+            //If any regular number is 10 or greater, the leftmost such regular number splits.
+            if (Number >= 10)
+            {
+                Split();
+                return true;
+            }
+            return false;
+        }
+
+        public void Split()
+        {
+            //To split a regular number, replace it with a pair;
+            //the left element of the pair should be the regular number divided by two and rounded down,
+            //while the right element of the pair should be the regular number divided by two and rounded up.
+            //For example, 10 becomes [5,5], 11 becomes [5,6], 12 becomes [6,6], and so on.
+            var half = Math.DivRem(Number, 2, out int remainder);
+            Number = half;
+            var right = new SnailfishRegularNumber(half + remainder);
+            var currentParent = Parent;
+            var currenPosition = NumberPosition;
+            SnailfishNumberPair replacement = new SnailfishNumberPair(this, right);
+            replacement.Parent = currentParent;
+            replacement.NumberPosition = currenPosition;
+            if (currenPosition == PairPosition.Left)
+            {
+                currentParent.LeftNumber = replacement;
+            }
+            else
+            {
+                currentParent.RightNumber = replacement;
+            }
+        }
+
+        public override void AddToLeft(int number)
+        {
+            Number += number;
+        }
+
+        public override void AddToRight(int number)
+        {
+            Number += number;
+        }
+    }
+
+    public abstract class SnailfishNumber
+    {
+        public SnailfishNumberPair Parent { get; set; }
+        public PairPosition NumberPosition { get; set; }
+        public abstract long Magnitude { get; }
+
+        public static SnailfishNumber operator +(SnailfishNumber a) => a;
+
+        public static SnailfishNumberPair operator +(SnailfishNumber left, SnailfishNumber right)
+            => new SnailfishNumberPair(left, right);
+
+        public abstract bool CheckExplode(int pairCount);
+        public abstract bool CheckSplit();
+
+        public abstract void AddToLeft(int number);
+        public abstract void AddToRight(int number);
+    }
+
+    public enum PairPosition
+    {
+        Unknown,
+        Left,
+        Right
+    }
+
+    [TestCategory("2021")]
+    [TestClass]
+    public class ReduceTester
+    {
+        [TestMethod]
+        public void SingleExplode()
+        {
+            //[[[[[9,8],1],2],3],4] => [[[[0,9],2],3],4]
+            var pair = new SnailfishNumberPair(new SnailfishRegularNumber(9), new SnailfishRegularNumber(8));
+            var pair2 = new SnailfishNumberPair(pair, new SnailfishRegularNumber(1));
+            var pair3 = new SnailfishNumberPair(pair2, new SnailfishRegularNumber(2));
+            var pair4 = new SnailfishNumberPair(pair3, new SnailfishRegularNumber(3));
+            var pair5 = new SnailfishNumberPair(pair4, new SnailfishRegularNumber(4));
+
+            pair5.Reduce();
+            Assert.AreEqual(true, pair2.LeftNumber is SnailfishRegularNumber);
+            Assert.AreEqual(true, pair2.RightNumber is SnailfishRegularNumber);
+            Assert.AreEqual(0, ((SnailfishRegularNumber)pair2.LeftNumber).Number);
+            Assert.AreEqual(9, ((SnailfishRegularNumber)pair2.RightNumber).Number);
+        }
+        [TestMethod]
+        public void SingleMagnitude()
+        {
+            //[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]
+            //    \1/   \2/     \3/   \4/       \5/   \6/    \7/
+            //     ***8***       ***9***         ***10**      *
+            //        ******11******                *****12****
+            //              ************13***************
+            var pair1 = new SnailfishNumberPair(new SnailfishRegularNumber(8), new SnailfishRegularNumber(7));
+            var pair2 = new SnailfishNumberPair(new SnailfishRegularNumber(7), new SnailfishRegularNumber(7));
+            var pair3 = new SnailfishNumberPair(new SnailfishRegularNumber(8), new SnailfishRegularNumber(6));
+            var pair4 = new SnailfishNumberPair(new SnailfishRegularNumber(7), new SnailfishRegularNumber(7));
+            var pair5 = new SnailfishNumberPair(new SnailfishRegularNumber(0), new SnailfishRegularNumber(7));
+            var pair6 = new SnailfishNumberPair(new SnailfishRegularNumber(6), new SnailfishRegularNumber(6));
+            var pair7 = new SnailfishNumberPair(new SnailfishRegularNumber(8), new SnailfishRegularNumber(7));
+
+            var pair8 = new SnailfishNumberPair(pair1, pair2);
+            var pair9 = new SnailfishNumberPair(pair3, pair4);
+            var pair10 = new SnailfishNumberPair(pair5, pair6);
+            var pair11 = new SnailfishNumberPair(pair8, pair9);
+            var pair12 = new SnailfishNumberPair(pair10, pair7);
+            var pair13 = new SnailfishNumberPair(pair11, pair12);
+
+            Assert.AreEqual(3488, pair13.Magnitude);
+        }
+    }
+}
