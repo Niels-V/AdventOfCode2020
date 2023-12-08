d28ce061e19a717fce7062a250df6113cba5a4f3
Niels Vrolijk <loather@hotmail.com>
2023-12-08 21:43:44 +0100

    Migrated to net8 Solved some days in 2023.

---
+++ 2023/Dag02/Program.cs
@@ -0,0 +1,164 @@
+﻿using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC
+{
+    [TestCategory("2023")]
+    [TestClass]
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = First("input.txt");
+            Console.WriteLine("Program succes, found result: {0}", result);
+
+            var result2 = Second("input.txt");
+            Console.WriteLine("Program succes, found result: {0}", result2);
+        }
+
+        static long First(string inputFile)
+        {
+            var parser = new GameParser();
+            var gameData = parser.ReadData(inputFile);
+            //there are only 12 red cubes, 13 green cubes, and 14 blue cubes
+            var validGameIds = gameData
+                .Where(game => game.IsValid(12, 13, 14))
+                .Select(game => game.Id)
+                .ToList();
+            
+            return validGameIds.Sum();
+        }
+
+        static long Second(string inputFile)
+        {
+            var parser = new GameParser();
+            var gameData = parser.ReadData(inputFile);
+            return gameData.Sum(game => game.FindPower());
+        }
+
+        [TestMethod]
+        public void TestPart1()
+        {
+            var result = First("test.txt");
+            Assert.AreEqual(8, result);
+        }
+
+        [TestMethod]
+        public void TestPart2()
+        {
+            var result = Second("test.txt");
+            Assert.AreEqual(2286, result);
+        }
+        [TestClass]
+        public class Game {
+            
+            public int Id {get;set;}
+            public List<GameSet> Sets {get;private set;} = new List<GameSet>();
+
+            public bool IsValid(int maxRed, int maxGreen, int maxBlue) {
+                return Sets.All(set=>set.Red <= maxRed && set.Blue <= maxBlue && set.Green <= maxGreen);
+            }
+            [TestMethod]
+            public void TestIsValid()
+            {
+                var game = new Game { Id = 13 };
+                game.Sets.Add(new GameSet { Blue = 3, Green = 12, Red = 12 });
+                game.Sets.Add(new GameSet { Blue = 3, Green = 5, Red = 2 });
+                game.Sets.Add(new GameSet { Blue = 7, Green = 3, Red = 13 });
+                game.Sets.Add(new GameSet { Blue = 0, Green = 4, Red = 7 });
+                game.Sets.Add(new GameSet { Blue = 3, Green = 3, Red = 5 });
+
+                Assert.AreEqual(false, game.IsValid(12, 13, 14));
+            }
+            public int MinRed { get; set; }
+            public int MinGreen { get; set; }
+            public int MinBlue { get; set; }
+            public long Power => MinRed * MinGreen * MinBlue;
+            public long FindPower()
+            {
+                MinRed = 0;
+                MinGreen = 0;
+                MinBlue = 0;
+                foreach (var set in Sets)
+                {
+                    MinRed = MinRed < set.Red ? set.Red : MinRed;
+                    MinGreen = MinGreen < set.Green ? set.Green : MinGreen;
+                    MinBlue = MinBlue < set.Blue ? set.Blue : MinBlue;
+                }
+                return Power;
+            }
+        }
+
+        public class GameSet {
+            public int Red {get;set;}
+            public int Green {get;set;}
+            public int Blue {get;set;}
+
+        }
+        [TestClass]
+        public class GameParser : Common.Parser<Game> {
+            protected override Game ParseLine(string line) {
+                // line: Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+                // split ------|-----------------------------------------------
+                var data = line.Split(':', StringSplitOptions.RemoveEmptyEntries);
+                var id = data[0].Substring(5);
+                var game = new Game{ Id=Convert.ToInt32(id)};
+                //data[1]:  3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+                //split:   --------------|-----------------------|--------
+
+                var sets = data[1].Split(';', StringSplitOptions.RemoveEmptyEntries);
+                foreach (var set in sets) {
+                    //set[0]:  3 blue, 4 red
+                    //split:  -------|------
+                    var setData = set.Split(',', StringSplitOptions.RemoveEmptyEntries);
+                    var setInfo = new GameSet();
+                    game.Sets.Add(setInfo);
+                    foreach(string colorInfo in setData) {
+                        var colorData = colorInfo.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                        switch (colorData[1]) {
+                            case "red":
+                                setInfo.Red = Convert.ToInt32(colorData[0]);
+                                break;
+                            case "green":
+                                setInfo.Green = Convert.ToInt32(colorData[0]);
+                                break;
+                            case "blue":
+                                setInfo.Blue = Convert.ToInt32(colorData[0]);
+                                break;
+                        }
+                    }
+
+                }
+                return game;
+           }
+           [TestMethod]
+           public void TestSet() {
+            string s = "Game 10: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+            var result = ParseLine(s);
+            Assert.AreEqual(10, result.Id);
+            Assert.AreEqual(3, result.Sets.Count());
+            Assert.AreEqual(0, result.Sets.First().Green);
+            Assert.AreEqual(3, result.Sets.First().Blue);
+            Assert.AreEqual(4, result.Sets.First().Red);
+
+           }
+            [TestMethod]
+            public void TestSet2()
+            {
+                string s = "Game 13: 3 blue, 12 red, 12 green; 5 green, 3 blue, 2 red; 3 green, 7 blue, 13 red; 4 green, 7 red; 3 green, 3 blue, 5 red";
+                var result = ParseLine(s);
+                Assert.AreEqual(13, result.Id);
+                Assert.AreEqual(5, result.Sets.Count());
+                Assert.AreEqual(12, result.Sets.First().Green);
+                Assert.AreEqual(3, result.Sets.First().Blue);
+                Assert.AreEqual(12, result.Sets.First().Red);
+
+            }
+        }
+    }
+}
---
+++ 2023/Dag07/Program.cs
@@ -0,0 +1,275 @@
+﻿using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.Serialization;
+
+namespace AoC
+{
+    [TestCategory("2023")]
+    [TestClass]
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = First("input.txt");
+            Console.WriteLine("Program succes, found result: {0}", result);
+
+            var result2 = Second("input.txt");
+            Console.WriteLine("Program succes, found result: {0}", result2);
+        }
+
+        static int First(string inputFile)
+        {
+            var parser = new HandParser();
+            var hands = parser.ReadData(inputFile);
+
+            var sorted = hands.OrderBy(h => h);
+            var result = sorted.Select((hand, index) => hand.Bid*(index+1)).Sum();
+            return result;
+        }
+
+        static long Second(string inputFile)
+        {
+            var parser = new HandWithJokerParser();
+            var hands = parser.ReadData(inputFile);
+
+            var sorted = hands.OrderBy(h => h);
+            var result = sorted.Select((hand, index) => hand.Bid*(index+1)).Sum();
+            return result;
+        }
+
+        [TestMethod]
+        public void TestPart1()
+        {
+            var result = First("test.txt");
+            Assert.AreEqual(6440, result);
+        }
+
+        [TestMethod]
+        public void TestPart2()
+        {
+            var result = Second("test.txt");
+            Assert.AreEqual(5905, result);
+        }
+    }
+    [TestClass]
+    public class HandParser : Common.Parser<Hand>
+    {
+        protected override Hand ParseLine(string line)
+        {
+            return new Hand(line);
+        }
+        [TestMethod]
+        [DataRow("AAAAA 1", HandType.FiveOfAKind, 1)]
+        [DataRow("AA8AA 1", HandType.FourOfAKind, 1)]
+        [DataRow("23332 1", HandType.FullHouse, 1)]
+        [DataRow("TTT98 1", HandType.ThreeOfAKind, 1)]
+        [DataRow("23432 1", HandType.TwoPair, 1)]
+        [DataRow("A23A4 1", HandType.OnePair, 1)]
+        [DataRow("23456 1", HandType.HighCard, 1)]
+        public void TestParse(string line, HandType expectedHand, int expectedBid)
+        {
+            var hand = new Hand(line);
+            Assert.AreEqual(expectedHand, hand.HandType);
+            Assert.AreEqual(expectedBid, hand.Bid);
+        }
+    }
+
+    [TestClass]
+    public class HandWithJokerParser : Common.Parser<Hand>
+    {
+        protected override Hand ParseLine(string line)
+        {
+            return new Hand(line, true);
+        }
+        [TestMethod]
+        [DataRow("AAAAA 1", HandType.FiveOfAKind, 1)]
+        [DataRow("AA8AA 1", HandType.FourOfAKind, 1)]
+        [DataRow("23332 1", HandType.FullHouse, 1)]
+        [DataRow("TTT98 1", HandType.ThreeOfAKind, 1)]
+        [DataRow("23432 1", HandType.TwoPair, 1)]
+        [DataRow("A23A4 1", HandType.OnePair, 1)]
+        [DataRow("23456 1", HandType.HighCard, 1)]
+        [DataRow("32T3K 765", HandType.OnePair, 765)]
+        [DataRow("T55J5 684", HandType.FourOfAKind, 684)]
+        [DataRow("KK677 28", HandType.TwoPair, 28)]
+        [DataRow("KTJJT 220", HandType.FourOfAKind, 220)]
+        [DataRow("QQQJA 483", HandType.FourOfAKind, 483)]
+        public void TestParse(string line, HandType expectedHand, int expectedBid)
+        {
+            var hand = new Hand(line,true);
+            Assert.AreEqual(expectedHand, hand.HandType);
+            Assert.AreEqual(expectedBid, hand.Bid);
+        }
+    }
+
+    public class Hand : IComparable<Hand>, IComparer<Hand> 
+    {
+        public bool JokerRules {get; private set;}
+        public HandType HandType { get; set; }
+        public CardType[] Cards { get; set; } = new CardType[5];
+        public int Bid { get; set; }
+        public Hand(string entryLine, bool withJokers = false)
+        {
+            JokerRules = withJokers;
+            var line = entryLine.Split(' ');
+            var handstring = line[0];
+            var bid = Convert.ToInt32(line[1]);
+            Bid = bid;
+            for (int i = 0; i < 5; i++)
+            {
+                Cards[i] = handstring[i].ToString().ParseEnumValue<CardType>();
+            }
+            HandType = DetermineHandType();
+        }
+
+        public HandType DetermineHandType()
+        {
+            var query = from r in Cards
+                        group r by r into g
+                        select new { Count = g.Count(), Value = g.Key };
+            if (query.Count()==1)
+            {
+                return HandType.FiveOfAKind;
+            }
+            if (query.Count() == 2 && (query.ElementAt(0).Count == 1|| query.ElementAt(0).Count == 4))
+            {
+                if (JokerRules && query.ElementAt(0).Value == CardType.Jack ||
+                    query.ElementAt(1).Value == CardType.Jack)
+                    {
+                        return HandType.FiveOfAKind;
+                    }
+                return HandType.FourOfAKind;
+            }
+            if (query.Count() == 2 && (query.ElementAt(0).Count == 2 || query.ElementAt(0).Count == 3))
+            {
+                if (JokerRules && query.ElementAt(0).Value == CardType.Jack ||
+                    query.ElementAt(1).Value == CardType.Jack)
+                    {
+                        return HandType.FiveOfAKind;
+                    }
+                return HandType.FullHouse;
+            }
+            if (query.Count() == 3 && (query.ElementAt(0).Count == 3 || query.ElementAt(1).Count == 3 || query.ElementAt(2).Count == 3))
+            {
+                if (JokerRules &&   
+                    (query.ElementAt(0).Value == CardType.Jack) ||
+                     (query.ElementAt(1).Value == CardType.Jack) ||
+                     (query.ElementAt(2).Value == CardType.Jack)
+                ) {
+                    return HandType.FourOfAKind;
+                }
+                return HandType.ThreeOfAKind;
+            }
+            if (query.Count() == 3 && (query.ElementAt(0).Count == 2 || query.ElementAt(1).Count == 2 || query.ElementAt(2).Count == 2))
+            {
+                if (JokerRules &&   (
+                     (query.ElementAt(0).Count==2 && query.ElementAt(0).Value == CardType.Jack) ||
+                     (query.ElementAt(1).Count==2 && query.ElementAt(1).Value == CardType.Jack) ||
+                     (query.ElementAt(2).Count==2 && query.ElementAt(2).Value == CardType.Jack)
+                )) {
+                    return HandType.FourOfAKind;
+                }
+                if (JokerRules &&  ( 
+                     (query.ElementAt(0).Count==1 && query.ElementAt(0).Value == CardType.Jack) ||
+                     (query.ElementAt(1).Count==1 && query.ElementAt(1).Value == CardType.Jack) ||
+                     (query.ElementAt(2).Count==1 && query.ElementAt(2).Value == CardType.Jack)
+                )) {
+                    return HandType.FullHouse;
+                }
+                return HandType.TwoPair;
+            }
+            if (query.Count() == 4)
+            {
+                if (JokerRules &&   (
+                     (query.ElementAt(0).Value == CardType.Jack) ||
+                     (query.ElementAt(1).Value == CardType.Jack) ||
+                     (query.ElementAt(2).Value == CardType.Jack) ||
+                     (query.ElementAt(3).Value == CardType.Jack))
+                ) {
+                    return HandType.ThreeOfAKind;
+                }
+                return HandType.OnePair;
+            }
+            if (JokerRules && (
+                     (query.ElementAt(0).Value == CardType.Jack) ||
+                     (query.ElementAt(1).Value == CardType.Jack) ||
+                     (query.ElementAt(2).Value == CardType.Jack) ||
+                     (query.ElementAt(3).Value == CardType.Jack) ||
+                     (query.ElementAt(4).Value == CardType.Jack))
+                ) {
+                    return HandType.OnePair;
+                }
+            return HandType.HighCard;
+        }
+
+        public int CompareTo(Hand other)
+        {
+            if (this.HandType == other.HandType)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    var compareResult = Cards[i].CompareTo(other.Cards[i]);
+                    if (JokerRules && compareResult!=0) {
+                        if (Cards[i] == CardType.Jack) {return -1;}
+                        if (other.Cards[i] == CardType.Jack) {return 1;}
+                    }
+                    if (compareResult !=0)
+                    {
+                        return compareResult;
+                    }
+                }
+            }
+            return HandType.CompareTo(other.HandType);
+        }
+
+        public int Compare(Hand x, Hand y)
+        {
+            return x.CompareTo(y);
+        }
+    }
+
+    public enum HandType {
+        HighCard = 1, //where all cards' labels are distinct: 23456
+        OnePair = 2, //where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
+        TwoPair = 3, //where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
+        ThreeOfAKind = 4, //where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
+        FullHouse = 5, //where three cards have the same label, and the remaining two cards share a different label: 23332
+        FourOfAKind = 6, //where four cards have the same label and one card has a different label: AA8AA
+        FiveOfAKind = 7,// where all five cards have the same label: AAAAA
+    }
+
+    public enum CardType
+    {
+        [EnumMember(Value = "A")] 
+        Ace = 14,
+        [EnumMember(Value = "K")] 
+        King = 13,
+        [EnumMember(Value = "Q")] 
+        Queen = 12,
+        [EnumMember(Value = "J")] 
+        Jack = 11,
+        [EnumMember(Value = "T")] 
+        Ten = 10,
+        [EnumMember(Value = "9")] 
+        Nine = 9,
+        [EnumMember(Value = "8")]
+        Eight = 8,
+        [EnumMember(Value = "7")]
+        Seven = 7,
+        [EnumMember(Value = "6")] 
+        Six = 6,
+        [EnumMember(Value = "5")] 
+        Five = 5,
+        [EnumMember(Value = "4")] 
+        Four = 4,
+        [EnumMember(Value = "3")]
+        Three = 3,
+        [EnumMember(Value = "2")]
+        Two = 2
+    }
+}
